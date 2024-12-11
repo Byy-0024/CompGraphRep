93b1c66efa1d7be33e1afa44ae73c4c94c43cb5c
Byy-0024 <byyang23@m.fudan.edu.cn>
2024-12-11 14:54:10 +0800

    Update pr_gpu.sh

--- scripts/pr_gpu.sh
+++ scripts/pr_gpu.sh
@@ -1,26 +1,25 @@
-ROOT=~/yby/HVI
+ROOT="$REPO_ROOT"
+DATAROOT="$DATA_ROOT"
+ORDERING=$1
+DEVICEID=$2
 TASK=pr
 EXE=${ROOT}/bin/${TASK}_gpu
-DATAROOT=/data/disk1/yangboyu/HVI/data
-ORDERING=Greedy
-DEVICEID=1
 GRAPHDIRS=(
-    bn-bnu \
-    bn-jung \
-    sc-ldoor \
-    sc-msdoor \
-    # sc-TSOPF \
-    sc-nasasrb \
-    tech-ip \
+    # bn-bnu \
+    # bn-jung \
+    # sc-ldoor \
+    # sc-msdoor \
+    # sc-nasasrb \
+    # tech-ip \
     web-arabic-2005 \
-    web-BerkStan-dir \
-    web-google-dir \
-    web-indochina-2004-all \
-    web-it-2004 \
-    web-it-2004-all \
-    web-Stanford \
-    web-uk-2002-all \
-    web-uk-2005 \
+    # web-BerkStan-dir \
+    # web-google-dir \
+    # web-indochina-2004-all \
+    # web-it-2004 \
+    # web-it-2004-all \
+    # web-Stanford \
+    # web-uk-2002-all \
+    # web-uk-2005 \
 )
 
 cd ${ROOT}
