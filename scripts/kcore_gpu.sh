c88ca3341821353647281da14d590145177aa092
Byy-0024 <byyang23@m.fudan.edu.cn>
2024-12-11 14:53:25 +0800

    Update kcore_gpu.sh

--- scripts/kcore_gpu.sh
+++ scripts/kcore_gpu.sh
@@ -1,30 +1,29 @@
-ROOT=~/yby/HVI
+ROOT="$REPO_ROOT"
+DATAROOT="$DATA_ROOT"
+ORDERING=$1
+DEVICEID=$2
 TASK=kcore
 EXE=${ROOT}/bin/${TASK}_gpu
-DATAROOT=/data/disk1/yangboyu/HVI/data
-ORDERING=origin
-DEVICEID=1
 GRAPHDIRS=(
-    bn-bnu \
-    bn-jung \
-    sc-ldoor \
-    sc-msdoor \
-    sc-nasasrb \
-    # sc-TSOPF \
-    tech-ip \
+    # bn-bnu \
+    # bn-jung \
+    # sc-ldoor \
+    # sc-msdoor \
+    # sc-nasasrb \
+    # tech-ip \
     web-arabic-2005 \
-    web-BerkStan-dir \
-    web-google-dir \
-    web-indochina-2004-all \
-    web-it-2004 \
-    web-it-2004-all \
-    web-Stanford \
-    web-uk-2002-all \
-    web-uk-2005 \
+    # web-BerkStan-dir \
+    # web-google-dir \
+    # web-indochina-2004-all \
+    # web-it-2004 \
+    # web-it-2004-all \
+    # web-Stanford \
+    # web-uk-2002-all \
+    # web-uk-2005 \
 )
 
 cd ${ROOT}
-# mkdir exp/${TASK}
+mkdir exp/gpu/${TASK}
 for GRAPHDIR in "${GRAPHDIRS[@]}"; do
     echo ${GRAPHDIR}
     ${EXE} ${DATAROOT}/${GRAPHDIR} ${ORDERING} ${DEVICEID} > ${ROOT}/exp/gpu/${TASK}/${GRAPHDIR}_${ORDERING}.log
